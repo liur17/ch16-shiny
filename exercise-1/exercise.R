3d3873ee47b00549fb13862f53f8710ef563439e
Tyler Vanbrocklin <tylervanbrocklin@D-173-250-206-186.dhcp4.washington.edu>
2017-11-14 13:58:34 -0800

    k:

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -4,6 +4,14 @@
 
 
 # Source your BuildScatter.r script, exposing your BuildScatter function
-
+source("scripts/BuildScatter.r")
 
 # Use your BuildScatter function to draw a well labeled ggplot scatterplot of the iris data
+BuildScatter(data = iris, 
+             xVar = 'Sepal.Length', 
+             yVar = 'Sepal.Width',
+             colorVar = 'Species', 
+             title = 'Iris Dataset', 
+             xLab = 'Sepal Length', 
+             yLab = 'Sepal Width'
+)
